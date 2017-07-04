e1884100bfa297a37997bf6e8e292407197f0623
Su Lianggang <94slgbest@163.com>
2017-07-04 22:33:27 +0800

    anchor postion
    anchor postion

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,7 +1,11 @@
 angular.module('controllerModule', ['angularTranslateModule'])
-    .controller('mainCtrl', function ($scope, $rootScope) {
+    .controller('mainCtrl', function ($scope, $rootScope, $location, $anchorScroll) {
+        $scope.anchor = function (anchor) {
+            $location.hash(anchor);
+            $anchorScroll();
+        }
 
     })
     .controller('homeCtrl', function ($scope, $rootScope) {
 
-    })
+})
