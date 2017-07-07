c9e6cd86818d24878cf298aad8f6f2adfc08ea58
时昌雪 <925290164@qq.com>
2017-07-07 16:23:58 +0800

    init

--- app/js/router.js
+++ app/js/router.js
@@ -6,6 +6,16 @@ angular.module('routerModule', ['controllerModule'])
                 templateUrl: 'app/views/main.html',
                 controller: 'mainCtrl'
             })
+            .state('contract', {
+                url: '/contract',
+                templateUrl: 'app/views/pragmasolidity.html',
+                controller: 'mainCtrl'
+            })
+            .state('explain', {
+                url: '/explain',
+                templateUrl: 'app/views/explain.html',
+                controller: 'mainCtrl'
+            })
             .state('app.home', {
                 url: '/home',
                 views: {
